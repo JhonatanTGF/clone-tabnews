e0f8b591309cb3095fc960cee70a1b6c9f00fc79
Jhonatan Gonçalves <41028169+JhonatanTGF@users.noreply.github.com>
2023-07-21 00:19:33 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um exercício de "Aprender a Programar" copiando o TabNews do FilipeDechamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Carol, eu amo você S2! Se você me ama, dá uma risadinha!</h1>
+  return <h1>Carol, eu amo você S2! Se você me ama, dá uma risadinha!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
